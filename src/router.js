d2d966ac584c990099d96dfc7938eea5026a9614
Jusce <cassio.juscelino@gmail.com>
2024-05-19 19:22:51 -0300

    feature: criação da base do calculo do projeto

--- src/router.js
+++ src/router.js
@@ -2,4 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
+const wttsController = require('./controllers/wttsController');
+
+router.get('/getWtts', wttsController.getJson);
+
+router.post('/postWtts', wttsController.calcWtts);
+
 module.exports = router;
