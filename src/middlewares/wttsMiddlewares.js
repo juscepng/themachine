3954ce1b3e20df99d1fc6121ad7b9c425f3fe0dc
Jusce <cassio.juscelino@gmail.com>
2024-05-24 13:55:50 -0300

    feat: adicionado e ajustado alguns testes - adicionado teste de validação de numero e melhoria no envio de msg do teste

--- src/middlewares/wttsMiddlewares.js
+++ src/middlewares/wttsMiddlewares.js
@@ -1,27 +1,40 @@
 const validateJson = (req, res, next) => {
     const { body } = req;
 
-    if (Object.keys(body).length === 0) return res.status(400).json({ error: 'Body não foi encontrado.' })
+    if (Object.keys(body).length === 0)
+        return sendMessage(res, 'Body não foi encontrado.')
 
-    if (!body.usuario) return res.status(400).json({ error: 'Usuário não foi encontrado.' })
+    if (!body.usuario)
+        return sendMessage(res, 'Usuário não foi encontrado.')
 
-    if (!body.concessionaria) return res.status(400).json({ error: 'Concessionaria não foi encontrado.' })
+    if (!body.concessionaria)
+        return sendMessage(res, 'Concessionaria não foi encontrada.')
 
-    if (body.local.length == 0) return res.status(400).json({ error: 'Local não foi encontrado.' })
+    if (!validNumber(body.concessionaria))
+        return sendMessage(res, 'O valor da concessionária informado não e válido.')
+
+    if (body.local.length == 0)
+        return sendMessage(res, 'Local não foi encontrado.')
 
     body.local.forEach(local => {
         if (!local.nome) {
-            return res.status(400).json({ error: 'Nome do local não foi encontrado.' })
+            return sendMessage(res, 'Nome do local não foi encontrado.')
         }
         else {
             const { equipamentos } = local;
 
             if (equipamentos.length == 0) {
-                return res.status(400).json({ error: 'Equipamentos não foi encontrado.' })
+                return sendMessage(res, 'Equipamentos não foi encontrado.')
             } else {
                 equipamentos.forEach(equipamento => {
                     for (let e in equipamento) {
-                        if (!equipamento[e]) return res.status(400).json({ error: `O(a) ${e} do equipamento não foi encontrado.` })
+                        if (!equipamento[e])
+                            return sendMessage(res, `O(a) ${e} do equipamento não foi encontrado.`)
+
+                        if (e != 'nome') {
+                            if (!validNumber(equipamento[e]))
+                                return sendMessage(res, `O valor do(a) ${e} do equipamento não é valido.`)
+                        }
                     }
                 });
             }
@@ -29,6 +42,14 @@ const validateJson = (req, res, next) => {
     })
 
     next()
+}
+
+const sendMessage = (res, message) => {
+    return res.status(400).json({ error: message })
+}
+
+const validNumber = (value) => {
+    return !isNaN(value) && value > 0;
 }
 
 module.exports = {
--- src/test/wttsMiddlewares.spec.js
+++ src/test/wttsMiddlewares.spec.js
@@ -22,7 +22,7 @@ const next = () => { };
 
 describe('teste da validação do middleware', () => {
     it('o body esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts = {}
         const req = {
             body: wtts
@@ -35,7 +35,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'Body não foi encontrado.' })
     }),
     it('o usuario esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.usuario = '';
 
         const req = {
@@ -49,7 +49,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'Usuário não foi encontrado.' })
     }),
     it('a concessionaria esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.concessionaria = '';
 
         const req = {
@@ -60,10 +60,38 @@ describe('teste da validação do middleware', () => {
         wttsMiddlewares.validateJson(req, resp, next);
 
         expect(resp._status).toBe(400);
-        expect(resp._json).toStrictEqual({ error: 'Concessionaria não foi encontrado.' })
+        expect(resp._json).toStrictEqual({ error: 'Concessionaria não foi encontrada.' })
+    }),
+    it('o valor da concessionaria nao e um numero', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.concessionaria = 'abc';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor da concessionária informado não e válido.' })
+    }),
+    it('o valor da concessionaria e negativo', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.concessionaria = '-1';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor da concessionária informado não e válido.' })
     }),
     it('o local esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local = [];
 
         const req = {
@@ -77,7 +105,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'Local não foi encontrado.' })
     }),
     it('o nome do local esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].nome = '';
 
         const req = {
@@ -91,7 +119,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'Nome do local não foi encontrado.' })
     }),
     it('os equipamentos estao vazios', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].equipamentos = [];
     
 
@@ -106,7 +134,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'Equipamentos não foi encontrado.' })
     }),
     it('o nome do equipamento esta vazio', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].equipamentos[0].nome = '';
 
         const req = {
@@ -120,7 +148,7 @@ describe('teste da validação do middleware', () => {
         expect(resp._json).toStrictEqual({ error: 'O(a) nome do equipamento não foi encontrado.' })
     }),
     it('a potencia do equipamento esta vazia', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].equipamentos[0].potencia = '';
 
         const req = {
@@ -133,8 +161,36 @@ describe('teste da validação do middleware', () => {
         expect(resp._status).toBe(400);
         expect(resp._json).toStrictEqual({ error: 'O(a) potencia do equipamento não foi encontrado.' })
     }),
+    it('a potencia do equipamento nao e um numero', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].potencia = 'abc';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) potencia do equipamento não é valido.' })
+    }),
+    it('a potencia do equipamento e negativa', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].potencia = '-1';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) potencia do equipamento não é valido.' })
+    }),
     it('as horas do equipamento estao vazias', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].equipamentos[0].horas = '';
 
         const req = {
@@ -147,8 +203,38 @@ describe('teste da validação do middleware', () => {
         expect(resp._status).toBe(400);
         expect(resp._json).toStrictEqual({ error: 'O(a) horas do equipamento não foi encontrado.' })
     }),
+    
+    it('as horas do equipamento nao sao um numero', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].horas = 'abc';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) horas do equipamento não é valido.' })
+    }),
+    
+    it('as horas do equipamento sao negativas', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].horas = '-1';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) horas do equipamento não é valido.' })
+    }),
     it('os dias do equipamento estao vazios', () => {
-        let wtts = JSON.parse(JSON.stringify(baseWtts));
+        let wtts = parseJson(baseWtts);
         wtts.local[0].equipamentos[0].dias = '';
 
         const req = {
@@ -160,6 +246,34 @@ describe('teste da validação do middleware', () => {
 
         expect(resp._status).toBe(400);
         expect(resp._json).toStrictEqual({ error: 'O(a) dias do equipamento não foi encontrado.' })
+    }),
+    it('os dias do equipamento nao e um numero', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].dias = 'abc';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) dias do equipamento não é valido.' })
+    }),
+    it('os dias do equipamento e negativo', () => {
+        let wtts = parseJson(baseWtts);
+        wtts.local[0].equipamentos[0].dias = '-1';
+
+        const req = {
+            body: wtts
+        };
+        const resp = new ResponseMock();
+
+        wttsMiddlewares.validateJson(req, resp, next);
+
+        expect(resp._status).toBe(400);
+        expect(resp._json).toStrictEqual({ error: 'O valor do(a) dias do equipamento não é valido.' })
     })
 })
 
@@ -175,4 +289,8 @@ class ResponseMock {
         this._json = value;
         return this;
     }
+}
+
+const parseJson = (value) => {
+    return JSON.parse(JSON.stringify(value))
 }
